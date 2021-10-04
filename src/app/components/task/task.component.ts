697ef050b91fcb5435e203191d59b5dba95b1814
Nikita <Xellenad54@gmail.com>
2021-10-04 17:57:25 +0300

    refactor: chenge style

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -6,9 +6,9 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss']
 })
-export class TaskComponent  {
+export class TaskComponent {
 
-  todo: string[] = ['lorem3', 'Lorem ipsum dolor.', 'Lorem ipsum', 'Lorem sum', 'Lorem ipsumLorem ipsum']
+  todo: string[] = [ 'Lorem ipsumLorem ipsum', 'Lorem ipsum dolor.', 'Lorem ipsum', 'Lorem sum', 'Lorem ipsumLorem ipsum']
   progress: string[] = []
   completed: string[] = []
   inputText: string = ''
