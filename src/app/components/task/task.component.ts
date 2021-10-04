68240b0f1c32e7cef591e76d37acbd40b77e5f26
Nikita <Xellenad54@gmail.com>
2021-10-04 15:27:58 +0300

    feat: add transferring items between lists

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss']
 })
-export class TaskComponent implements OnInit {
+export class TaskComponent  {
 
-  constructor() { }
+  todo: string[] = ['lorem3', 'Lorem ipsum dolor.', 'Lorem ipsum', 'Lorem sum', 'Lorem ipsumLorem ipsum']
+  progress: string[] = []
+  completed: string[] = []
 
-  ngOnInit(): void {
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
   }
-
 }
