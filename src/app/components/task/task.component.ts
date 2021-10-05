6d14bb78e7fb4e4ae3930fb72682cf16d035f8a0
Nikita <Xellenad54@gmail.com>
2021-10-05 14:16:23 +0300

    refactor: add todo.interface

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 // @ts-ignore
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv } from 'uuid';
 
 import { LocalStorageService } from '../../shared/local-storage.service';
+import { Todo } from '../../shared/todo.interface';
 
 @Component({
   selector: 'app-task',
@@ -12,15 +13,21 @@ import { LocalStorageService } from '../../shared/local-storage.service';
 })
 export class TaskComponent  {
 
-  todo: string[] = [ 'Lorem ipsumLorem ipsum', 'Lorem ipsum dolor.', 'Lorem ipsum', 'Lorem sum', 'Lorem ipsumLorem ipsum']
-  progress: string[] = []
-  completed: string[] = []
+  todos: Todo[] = [
+    {text: 'Lorem ipsum dolor sit', id: 1},
+    {text: 'Lorem ipsum sit', id: 2},
+    {text: 'Lorem ipsum dolor ', id: 3},
+    {text: 'Lorem  dolor sit', id: 4},
+    {text: 'ipsum dolor sit', id: 5}
+  ]
+  progress: Todo[] = []
+  completed: Todo[] = []
   inputText: string = ''
 
   constructor(private localStorageService : LocalStorageService) {}
 
 
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<Todo[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -33,7 +40,11 @@ export class TaskComponent  {
 
   addTodo() {
     if (this.inputText.trim()) {
-      this.todo.unshift(this.inputText)
+      const todo: Todo = {
+        text: this.inputText,
+        id: uuidv()
+      }
+      this.todos.unshift(todo)
       this.inputText = ''
     }
   }
